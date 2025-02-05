dc25cb054aa4ccce142eb72c3af35454891b5191
Daniel Tan <danieltan.singapore@gmail.com>
2025-02-05 11:00:11 +0800

    Change analytics

--- main.py
+++ main.py
@@ -75,11 +75,7 @@ fonts = [
 hdrs = (
     picolink,
     Link(rel="stylesheet", href="https://unpkg.com/blocks.css/dist/blocks.min.css"),
-    Script(
-        defer="",
-        data_domain="ddanieltan.dev",
-        src="https://plausible.ddanieltan.dev/js/script.js",
-    ),
+    Script(data_goatcounter='https://ddanieltan.goatcounter.com/count', async='', src='//gc.zgo.at/count.js'),
     og_meta_tags,
     *fonts,
     css,
